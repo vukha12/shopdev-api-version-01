52b5a4b5acecc9e64e2241eac4fc0bd2c4896e77
vukha12 <kha15964@gmail.com>
2026-03-16 19:47:40 +0700

    Fix: change port

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const app = require('./src/app')
 
 
 
-const {PORT} = process.env;
+// const {PORT} = process.env;
+
+const PORT = 3000;
 
 
 
-
-const server = app.listen( PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`WSV start with port ${PORT}`);
 })
 
 process.on('SIGINT', () => {
-    server.close( () => console.log(`exits server express`))
+    server.close(() => console.log(`exits server express`))
 })
